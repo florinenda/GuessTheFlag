c88e90f8af2f1e32dd3492f57670445269a55fac
hillarynda <hillarynda0@gmail.com>
2023-11-27 19:58:54 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Apple Dev on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
